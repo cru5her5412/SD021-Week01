ef85a3fa5402703f5fef72c7f5fac74cbb4a87bc
Callum James Lodge <callumjlodge@gmail.com>
2025-08-13 17:22:58 +0100

    Changes made for day 3

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 function setup() {
-  createCanvas(400, 400);
+  ScreenHeight = 400;
+  ScreenWidth = 600;
+  createCanvas(ScreenWidth, ScreenHeight);
   rectMode(CENTER);
   PX = width / 2;
   PY = height / 2;
@@ -15,6 +17,13 @@ function draw() {
   createPlatform();
   createPlayer();
   MovePlayer();
+  fill(255, 0, 0);
+  text(
+    PlatCentreX + PlatWidth / 2 > PX && PX > PlatCentreX - PlatWidth / 2,
+    PlatCentreX + PlatWidth / 2,
+    100
+  );
+  fill(255);
 }
 function createPlatform() {
   fill(255, 0, 0);
@@ -27,23 +36,42 @@ function createPlayer() {
   fill(255);
 }
 function MovePlayer() {
-  // a = (((PlatCentreX+(PlatWidth/2))<PX<(PlatCentreX-(PlatWidth/2)))&&((PlatCentreY-(PlatHeight/2))>PY>(PlatCentreY+(PlatHeight/2))))
-
   if (
-    PlatCentreX + PlatWidth / 2 < PX < PlatCentreX - PlatWidth / 2 &&
-    PY + 10 > PlatCentreY - PlatHeight / 2
+    PlatCentreX + PlatWidth / 2 + 10 > PX &&
+    PX > PlatCentreX - PlatWidth / 2 - 10 &&
+    PY + 10 > PlatCentreY - PlatHeight / 2 &&
+    PY - 10 <= PlatCentreY + PlatHeight / 2
   ) {
+    if (PY + 10 + PAcc + 0.7 >= PlatCentreY - PlatHeight / 2) {
+      PAcc = 0;
+      PY = PlatCentreY - PlatHeight / 2 - 10;
+    }
     PAcc = 0;
   } else {
     if (PAcc > 7 || PAcc + 0.7 > 7) {
       PAcc = 7;
-    }
-    if (PY + 10 + PAcc + 0.7 >= PlatCentreY - PlatHeight / 2) {
-      PAcc = 0;
-      PY = PlatCentreY - PlatHeight / 2 - 10;
     } else {
       PAcc = PAcc + 0.7;
     }
     PY = PY + PAcc;
+  }
+  if (keyIsDown(65)) {
+    //left/a
+    PX = PX - 10;
+  }
+  if (keyIsDown(68)) {
+    PX = PX + 10;
+  }
+  if (PX < -10) {
+    PX = ScreenWidth + 5;
+  }
+  if (PX > ScreenWidth + 10) {
+    PX = -5;
+  }
+  if (PY < -10) {
+    PY = ScreenHeight + 5;
+  }
+  if (PY > ScreenHeight + 10) {
+    PY = -5;
   }
 }
