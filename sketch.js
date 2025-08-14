7a346c689e44d1f380fcf4fed7da5db58c243584
Callum James Lodge <callumjlodge@gmail.com>
2025-08-14 15:00:34 +0100

    Added todays work, and added support for multiple platforms using arrays

--- sketch.js
+++ sketch.js
@@ -5,30 +5,30 @@ function setup() {
   rectMode(CENTER);
   PX = width / 2;
   PY = height / 2;
-  PlatCentreX = 200;
-  PlatCentreY = 380;
-  PlatWidth = 360;
-  PlatHeight = 20;
+  PlatCentreX = [200, 400];
+  PlatCentreY = [300, 40];
+  PlatWidth = [360, 360];
+  PlatHeight = [20, 20];
   PAcc = 0;
+  testarray = ["1", "2", "3", "4"];
 }
 
 function draw() {
+  // fill(255,0,0);
+
   background(220);
+  //text((testarray.length), 100, 100);
+  //fill(255)
   createPlatform();
   createPlayer();
   MovePlayer();
-  fill(255, 0, 0);
-  text(
-    PlatCentreX + PlatWidth / 2 > PX && PX > PlatCentreX - PlatWidth / 2,
-    PlatCentreX + PlatWidth / 2,
-    100
-  );
-  fill(255);
 }
 function createPlatform() {
-  fill(255, 0, 0);
-  rect(PlatCentreX, PlatCentreY, PlatWidth, PlatHeight);
-  fill(255);
+  for (i = 0; i < PlatCentreY.length; i++) {
+    fill(255, 0, 0);
+    rect(PlatCentreX[i], PlatCentreY[i], PlatWidth[i], PlatHeight[i]);
+    fill(255);
+  }
 }
 function createPlayer() {
   fill(0, 0, 255);
@@ -36,42 +36,44 @@ function createPlayer() {
   fill(255);
 }
 function MovePlayer() {
-  if (
-    PlatCentreX + PlatWidth / 2 + 10 > PX &&
-    PX > PlatCentreX - PlatWidth / 2 - 10 &&
-    PY + 10 > PlatCentreY - PlatHeight / 2 &&
-    PY - 10 <= PlatCentreY + PlatHeight / 2
-  ) {
-    if (PY + 10 + PAcc + 0.7 >= PlatCentreY - PlatHeight / 2) {
+  for (i = 0; i < PlatCentreY.length; i++) {
+    if (
+      PlatCentreX[i] + PlatWidth[i] / 2 + 10 > PX &&
+      PX > PlatCentreX[i] - PlatWidth[i] / 2 - 10 &&
+      PY + 10 > PlatCentreY[i] - PlatHeight[i] / 2 &&
+      PY - 10 <= PlatCentreY[i] + PlatHeight[i] / 2
+    ) {
+      if (PY + 10 + PAcc + 0.7 >= PlatCentreY[i] - PlatHeight[i] / 2) {
+        PAcc = 0;
+        PY = PlatCentreY[i] - PlatHeight[i] / 2 - 10;
+      }
       PAcc = 0;
-      PY = PlatCentreY - PlatHeight / 2 - 10;
-    }
-    PAcc = 0;
-  } else {
-    if (PAcc > 7 || PAcc + 0.7 > 7) {
-      PAcc = 7;
     } else {
-      PAcc = PAcc + 0.7;
+      if (PAcc > 7 || PAcc + 0.7 > 7) {
+        PAcc = 7;
+      } else {
+        PAcc = PAcc + 0.7;
+      }
+      PY = PY + PAcc;
     }
-    PY = PY + PAcc;
-  }
-  if (keyIsDown(65)) {
-    //left/a
-    PX = PX - 10;
-  }
-  if (keyIsDown(68)) {
-    PX = PX + 10;
-  }
-  if (PX < -10) {
-    PX = ScreenWidth + 5;
-  }
-  if (PX > ScreenWidth + 10) {
-    PX = -5;
-  }
-  if (PY < -10) {
-    PY = ScreenHeight + 5;
-  }
-  if (PY > ScreenHeight + 10) {
-    PY = -5;
+    if (keyIsDown(65)) {
+      //left/a
+      PX = PX - 10;
+    }
+    if (keyIsDown(68)) {
+      PX = PX + 10;
+    }
+    if (PX < -10) {
+      PX = ScreenWidth + 5;
+    }
+    if (PX > ScreenWidth + 10) {
+      PX = -5;
+    }
+    if (PY < -10) {
+      PY = ScreenHeight + 5;
+    }
+    if (PY > ScreenHeight + 10) {
+      PY = -5;
+    }
   }
 }
